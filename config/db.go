b08c50f81156be3993e25fc43db7373f0e32f2ae
Muhammad Fadly <muhammad.fadly@work.bri.co.id>
2022-10-05 00:52:02 +0700

    show error mongo

--- config/db.go
+++ config/db.go
@@ -44,7 +44,7 @@ func DB() *mongo.Database {
 
 	if err := client.Ping(context.TODO(), readpref.Primary()); err != nil {
 		fmt.Println("mongodb failed...")
-		// log.Panic(err)
+		fmt.Println(err)
 	}
 	return client.Database("dashboard")
 }
